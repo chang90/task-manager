27da4b12890ee76a4a80a51824d6fc3997c82c44
hiby90hou <chang.liu.programmer@gmail.com>
2020-12-31 23:03:55 +1100

    able to sort and pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -28,10 +28,33 @@ router.post('/tasks', auth, async (req,res) => {
 })
 
 
-
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=createdAt:asc
 router.get('/tasks', auth, async (req, res) => {
+  const match = {}
+  const sort = {}
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true'
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':')
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+  }
+
+  req.query.completed
   try {
-    await req.user.populate('tasks').execPopulate()
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort
+      }
+    }).execPopulate()
     res.send(req.user.tasks)
   } catch (e) {
     console.log(e)
