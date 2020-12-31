59277223f74bd6cc7ed1b310232c7002e7413002
hiby90hou <chang.liu.programmer@gmail.com>
2021-01-01 00:11:58 +1100

    able to store image in local folder

--- src/index.js
+++ src/index.js
@@ -41,3 +41,25 @@ app.listen(port, () => {
 // }
 
 // main()
+
+const multer = require('multer')
+
+const upload = multer({
+  dest:'images',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, callback) {
+    if(!file.originalname.match(/\.(doc|docx)$/)) {
+      return callback(new Error('File must be in word document'))
+    }
+    callback(undefined, true)
+  }
+})
+
+
+app.post('/upload', upload.single('upload'), (req, res) =>{
+  res.send()
+}, (error,req,res,next) =>{
+  res.status(400).send({error: error.message})
+})
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = new express.Router()
+const multer = require('multer')
 const User = require('../models/user')
 const ObjectID = require('mongodb').ObjectID
 const auth = require('../middleware/auth')
+
 
 router.post('/users', async (req,res) => {
   const user = new User(req.body)
@@ -135,6 +137,25 @@ router.patch('/users/me', auth, async (req,res) =>{
     res.status(500).send()
   }
   
+})
+
+const upload = multer({
+  dest:'avatars',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, callback) {
+    if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return callback(new Error('File must be a image'))
+    }
+    callback(undefined, true)
+  }
+})
+
+router.post('/users/me/avatar',upload.single('avatar'), (req, res) =>{
+  res.send()
+}, (error,req,res,next) =>{
+  res.status(400).send({error: error.message})
 })
 
 module.exports = router
