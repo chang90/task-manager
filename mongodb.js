f89525ad8036fbb38b43f3f23193f068d4b1444d
hiby90hou <chang.liu.programmer@gmail.com>
2021-01-01 23:57:33 +1100

    add MONGODB_URL into env config

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 const {MongoClient, ObjectID} = require('mongodb')
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
+const connectionURL = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 const id = new ObjectID()
